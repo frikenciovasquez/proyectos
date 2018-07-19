faed64cd485aa532f079c7d342923c0c59d82e69
Frikencio <sanda9397@gmail.com>
2018-07-19 09:57:19 -0500

    Add files via upload

---
+++ proyecto/javascript/javascript.js
@@ -0,0 +1,101 @@
+var texto="";
+
+
+function displaytexto (boton1) {
+	
+
+	document.getElementById("display").value=prompt("escriba el texto");
+	texto=document.getElementById("display");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+
+function displaytexto2 (boton1) {
+	
+
+	document.getElementById("display2").value=prompt("escriba el texto");
+	texto=document.getElementById("display2");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+function displaytexto3 (boton1) {
+	
+
+	document.getElementById("display3").value=prompt("escriba el texto");
+	texto=document.getElementById("display3");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+function displaytexto4 (boton1) {
+	
+
+	document.getElementById("display4").value=prompt("escriba el texto");
+	texto=document.getElementById("display4");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+function displaytexto5 (boton1) {
+	
+
+	document.getElementById("display5").value=prompt("escriba el texto");
+	texto=document.getElementById("display5");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+function displaytexto6 (boton1) {
+	
+
+	document.getElementById("display6").value=prompt("escriba el texto");
+	texto=document.getElementById("display6");
+	//var editado=prompt("escribite el objetivo pibe")
+	// body... 
+}
+
+function cambiocolor(){
+	alert("hecho")
+	var cambio=document.getElementById("display");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+function cambiocolor2(){
+	alert("hecho")
+	var cambio=document.getElementById("display2");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+function cambiocolor3(){
+	alert("hecho")
+	var cambio=document.getElementById("display3");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+function cambiocolor4(){
+	alert("hecho")
+	var cambio=document.getElementById("display4");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+function cambiocolor5(){
+	alert("hecho")
+	var cambio=document.getElementById("display5");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+function cambiocolor6(){
+	alert("hecho")
+	var cambio=document.getElementById("display6");
+	cambio.classList.toggle("cambiodepost");
+}
+
+
+
